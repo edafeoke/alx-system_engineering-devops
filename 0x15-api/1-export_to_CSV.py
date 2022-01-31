085b3a13ca744d45191f7e978c6c98aaecd29180
Oke Edafe Great <greatedafeoke@gmail.com>
2022-01-31 01:20:19 -0800

    Added 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 '''
-for a given employee ID, export information about his/her TODO list progress
-to csv file
+for a given employee ID, returns information about his/her TODO list progress
 '''
 
 import csv
-import json
+import requests
 from sys import argv
-from urllib.request import urlopen
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'
-    url = url.format(argv[1])
-    with urlopen(url) as response:
-        encoding = response.headers.get_content_charset("utf-8")
-        data = response.read()
-        data = data.decode(encoding)
-        todos = json.loads(data)
-        done = 0
-        total = len(todos)
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todos?userId={}".format(argv[1])).json()
+    with open('{}.csv'.format(argv[1]), 'w', newline='') as csvfile:
+        my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            if todo["completed"] is True:
-                done += 1
-        text = "Employee EMPLOYEE_NAME is done with tasks({}/{}):"
-        print(text.format(done, total))
-        for todo in todos:
-            if todo["completed"]:
-                print("\t {}".format(todo["title"]))
+            my_writer.writerow([argv[1], user.get('username'),
+                                todo.get('completed'),
+                                todo.get('title')])
