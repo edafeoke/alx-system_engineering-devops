a18ea719f92539d165b99e8f7ee6774287fcabed
Oke Edafe Great <greatedafeoke@gmail.com>
2022-01-31 00:57:50 -0800

    Added 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,19 @@ for a given employee ID, returns information about his/her TODO list progress
 '''
 
 import json
+import requests
 from sys import argv
-from urllib.request import urlopen
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'
-    url = url.format(argv[1])
-    with urlopen(url) as response:
-        encoding = response.headers.get_content_charset("utf-8")
-        data = response.read()
-        data = data.decode(encoding)
-        todos = json.loads(data)
-        done = 0
-        total = len(todos)
-        for todo in todos:
-            if todo["completed"] is True:
-                done += 1
-        text = "Employee EMPLOYEE_NAME is done with tasks({}/{}):"
-        print(text.format(done, total))
-        for todo in todos:
-            if todo["completed"]:
-                print("\t {}".format(todo["title"]))
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todos?userId={}".format(argv[1])).json()
+    done = []
+    total = len(todos)
+    for todo in todos:
+        if todo.get("completed") is True:
+                done.append(todo.get("title"))
+    text = "Employee {} is done with tasks({}/{}):"
+    print(text.format(user.get("name"), len(done), total))
+    for todo in done:
+        print("\t {}".format(todo))
